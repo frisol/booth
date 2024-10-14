c582ecde3e9b929b13c1e38b698cf72a63e76394
Ben Hasler <ben.hasler@microsoft.com>
2024-10-14 21:46:02 +1300

    Refactor GPIO cleanup in booth.py

--- booth.py
+++ booth.py
@@ -1,11 +1,18 @@
-import RPi.GPIO as GPIO
+# Author: Ben Hasler
+# Date: 14/10/2024
+# Description: This script is used to control the photo booth. It will take photos and save them to a folder on the Raspberry Pi.
+
+# Import the necessary libraries
+# import RPi.GPIO as GPIO
+import os
+import glob
+import time
+import traceback
+from time import sleep
+
 
 # Set up GPIO mode
 GPIO.setmode(GPIO.BCM)
 
 # Clean up GPIO settings
 GPIO.cleanup()
-
-# Just another commit
-# Another commit
-# Another commit
