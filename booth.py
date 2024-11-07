a4262f47377f7c4637aeea4911b5930e5748108e
Ben Hasler <ben.hasler@microsoft.com>
2024-11-07 16:34:35 +1300

    Commented out line 71 - full screen due to error on big monitor

--- booth.py
+++ booth.py
@@ -68,7 +68,7 @@ pygame.display.set_mode((config.monitor_w, config.monitor_h))
 screen = pygame.display.get_surface()
 pygame.display.set_caption('Photo Booth Pics')
 pygame.mouse.set_visible(False) #hide the mouse cursor
-pygame.display.toggle_fullscreen()
+# pygame.display.toggle_fullscreen()
 
 #################
 ### Functions ###
