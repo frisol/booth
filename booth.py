4daa4ab529573f8c69e2acc5894a3f46adcd5016
Ben Hasler <ben.hasler@microsoft.com>
2024-11-08 10:05:34 +1300

    Added the main program start

--- booth.py
+++ booth.py
@@ -184,6 +184,8 @@ def display_pics(jpg_group, rotate, img_scale):
 def start_photobooth(): 
 
     create_pics_folder(config.file_path)
+    create_pics_folder(config.display_path)
+    create_pics_folder(config.pose_path)
 
     input(pygame.event.get()) # press escape to exit pygame. Then press ctrl-c to exit python.
 
@@ -266,6 +268,41 @@ def start_photobooth():
             camera.stop_preview()
             camera.close()
 
-create_pics_folder(config.file_path)
-create_pics_folder(config.display_path)
-create_pics_folder(config.pose_path)
+
+########################### Begin Step 3 #################################
+
+    input(pygame.event.get()) # press escape to exit pygame. Then press ctrl-c to exit python.
+	
+    if config.post_online:
+	    show_image(real_path + "/uploading.png")
+    else:
+	    show_image(real_path + "/processing.jpg")
+         
+
+####################
+### Main Program ###
+####################
+
+## clear the previously stored pics based on config settings
+if config.clear_on_startup:
+	clear_pics(1)
+
+print ("Photo booth app running...")
+for x in range(0, 5): #blink light to show the app is running
+	GPIO.output(led_pin,True)
+	sleep(0.25)
+	GPIO.output(led_pin,False)
+	sleep(0.25)
+
+## show_image(real_path + "/intro.png");
+## show_image(real_path + "/pics/2020-06-27-21-54-27-01-blur+text.jpg");
+
+while True:
+	## show_image(config.display_path + "2020-06-27-22-17-02-01-blur+text.jpg");
+	show_image(config.display_path + random.choice(os.listdir(config.display_path)))
+	## random_file=random.choice(os.listdir("Folder_Destination"))
+	GPIO.output(led_pin,True); #turn on the light showing users they can push the button
+	input(pygame.event.get()) # press escape to exit pygame. Then press ctrl-c to exit python.
+	GPIO.wait_for_edge(btn_pin, GPIO.FALLING)
+	time.sleep(config.debounce) #debounce
+	start_photobooth()
